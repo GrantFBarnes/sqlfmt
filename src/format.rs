c315609e51dc95e96551ca98eb31f9a4538f379d
Grant Barnes <gfbarnes78@gmail.com>
2025-04-20 16:22:39 -0500

    add basic formatting

--- src/format.rs
+++ src/format.rs
@@ -1,7 +1,61 @@
 use crate::token::*;
 
 pub fn get_formatted_sql(sql: String) -> String {
+    let mut result_builder: Vec<String> = vec![];
+
     let tokens: Vec<Token> = get_sql_tokens(sql);
-    dbg!(tokens);
-    return String::new();
+    for i in 0..tokens.len() {
+        let pre_space: Option<String> = get_pre_space(&tokens, i);
+        if pre_space.is_some() {
+            result_builder.push(pre_space.unwrap());
+        }
+        result_builder.push(tokens[i].value.clone());
+    }
+
+    return result_builder.concat().trim().to_string();
+}
+
+fn get_pre_space(tokens: &Vec<Token>, curr_idx: usize) -> Option<String> {
+    let token: &Token = &tokens[curr_idx];
+
+    if curr_idx == 0 || token.category == Some(TokenCategory::NewLine) {
+        return None;
+    }
+
+    let prev_token: &Token = &tokens[curr_idx - 1];
+
+    if prev_token.category == Some(TokenCategory::NewLine) {
+        return None;
+    }
+
+    return Some(String::from(" "));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_formatted_sql_basic() {
+        assert_eq!(
+            String::from("SELECT * FROM TBL1"),
+            get_formatted_sql(String::from("SELECT * FROM TBL1"))
+        );
+    }
+
+    #[test]
+    fn test_get_formatted_sql_basic_newlines() {
+        assert_eq!(
+            String::from(
+                r#"SELECT *
+FROM TBL1"#
+            ),
+            get_formatted_sql(String::from(
+                r#"
+                    SELECT  *
+                    FROM  TBL1
+                "#
+            ))
+        );
+    }
 }
