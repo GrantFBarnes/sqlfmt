ab7f9cb088715e16a250b5976b81b4859f17de52
Grant Barnes <gfbarnes78@gmail.com>
2025-04-20 17:05:40 -0500

    add format state, handle indent level

--- src/format.rs
+++ src/format.rs
@@ -1,31 +1,93 @@
 use crate::token::*;
 
+const INDENT_SIZE: usize = 4;
+
+struct FormatState {
+    result_builder: Vec<String>,
+    indent_stack: Vec<String>,
+}
+
+impl FormatState {
+    fn new() -> FormatState {
+        FormatState {
+            result_builder: vec![],
+            indent_stack: vec![],
+        }
+    }
+
+    fn is_line_start(&self) -> bool {
+        self.result_builder.last() == Some(&String::from("\n"))
+    }
+
+    fn get_indent(&self) -> String {
+        " ".repeat(INDENT_SIZE * self.indent_stack.len())
+    }
+
+    fn get_indent_stack(&self, token_value: String) -> Vec<String> {
+        let mut new_indent_stack: Vec<String> = self.indent_stack.clone();
+        match token_value.as_str() {
+            "SELECT" => {
+                new_indent_stack.push(token_value);
+            }
+            "FROM" => loop {
+                let top: Option<String> = new_indent_stack.pop();
+                if top.is_none() {
+                    break;
+                }
+
+                let top: String = top.unwrap();
+                let top: &str = top.as_str();
+                if top == "SELECT" {
+                    break;
+                }
+            },
+            _ => (),
+        }
+        return new_indent_stack;
+    }
+
+    fn get_result(&self) -> String {
+        self.result_builder.concat().trim().to_string()
+    }
+}
+
 pub fn get_formatted_sql(sql: String) -> String {
-    let mut result_builder: Vec<String> = vec![];
+    let mut state: FormatState = FormatState::new();
 
     let tokens: Vec<Token> = get_sql_tokens(sql);
     for i in 0..tokens.len() {
-        let pre_space: Option<String> = get_pre_space(&tokens, i);
+        let token: &Token = &tokens[i];
+
+        state.indent_stack = state.get_indent_stack(token.value.clone());
+
+        let pre_space: Option<String> = get_pre_space(&state, token);
         if pre_space.is_some() {
-            result_builder.push(pre_space.unwrap());
+            state.result_builder.push(pre_space.unwrap());
         }
-        result_builder.push(tokens[i].value.clone());
+
+        state.result_builder.push(token.value.clone());
     }
 
-    return result_builder.concat().trim().to_string();
+    return state.get_result();
 }
 
-fn get_pre_space(tokens: &Vec<Token>, curr_idx: usize) -> Option<String> {
-    let token: &Token = &tokens[curr_idx];
-
-    if curr_idx == 0 || token.category == Some(TokenCategory::NewLine) {
-        return None;
+fn get_pre_space(state: &FormatState, token: &Token) -> Option<String> {
+    match token.category {
+        Some(TokenCategory::NewLine)
+        | Some(TokenCategory::Delimiter)
+        | Some(TokenCategory::Comma) => {
+            return None;
+        }
+        Some(TokenCategory::Operator)
+        | Some(TokenCategory::Compare)
+        | Some(TokenCategory::Bitwise) => {
+            return Some(String::from(" "));
+        }
+        _ => (),
     }
 
-    let prev_token: &Token = &tokens[curr_idx - 1];
-
-    if prev_token.category == Some(TokenCategory::NewLine) {
-        return None;
+    if state.is_line_start() {
+        return Some(state.get_indent());
     }
 
     return Some(String::from(" "));
@@ -54,6 +116,28 @@ FROM TBL1"#
                 r#"
                     SELECT  *
                     FROM  TBL1
+                "#
+            ))
+        );
+    }
+
+    #[test]
+    fn test_get_formatted_sql_multiple_columns() {
+        assert_eq!(
+            String::from(
+                r#"SELECT
+    C1 AS 'Column 1',
+    C2 AS 'Column 2',
+    C3
+FROM TBL1 AS T"#
+            ),
+            get_formatted_sql(String::from(
+                r#"
+                    SELECT
+                    C1 AS 'Column 1',
+                    C2 AS 'Column 2',
+                    C3
+                    FROM TBL1 AS T
                 "#
             ))
         );
