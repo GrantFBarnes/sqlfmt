1543473f8d79f399ccca571e3d99673569e2a95a
Grant Barnes <gfbarnes78@gmail.com>
2025-04-21 17:26:03 -0500

    read stdin to format

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use std::io::{self, Stdin};
+
 use format::*;
 
 mod format;
 mod token;
 
 fn main() {
-    let sql: String = String::from(
-        r#"
-            SELECT *
-            FROM TBL1
-        "#,
-    );
-    let formatted: String = get_formatted_sql(sql);
-    dbg!(formatted);
+    let stdin: Stdin = io::stdin();
+    let sql: Result<String, io::Error> = io::read_to_string(stdin);
+    if sql.is_err() {
+        panic!("Failed to read input to string.");
+    }
+    let sql: String = sql.unwrap();
+
+    let formatted_sql: String = get_formatted_sql(sql);
+    println!("{}", formatted_sql);
 }
