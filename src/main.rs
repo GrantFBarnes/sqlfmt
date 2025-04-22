d4536855615e346c1ab1c78aeb98132ae7e926c8
Grant Barnes <gfbarnes78@gmail.com>
2025-04-21 19:32:31 -0500

    redesign args to define settings

--- src/main.rs
+++ src/main.rs
@@ -15,57 +15,139 @@ const FONT_MAGENTA: &str = "\x1b[35m";
 const FONT_YELLOW: &str = "\x1b[33m";
 const FONT_RESET: &str = "\x1b[0m";
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.contains(&String::from("-h")) || args.contains(&String::from("--help")) {
-        print_help();
-        process::exit(0);
+struct Settings {
+    input: Option<String>,
+    output: Option<String>,
+    case: Option<CaseSetting>,
+}
+
+impl Settings {
+    fn new() -> Settings {
+        Settings {
+            input: None,
+            output: None,
+            case: None,
+        }
     }
+}
 
-    let sql: Option<String>;
-    let mut output_path: Option<String> = None;
+#[derive(PartialEq, Eq)]
+enum CaseSetting {
+    Upper,
+    Lower,
+}
 
-    let stdin: Stdin = io::stdin();
-    if stdin.is_terminal() {
-        match args.len() {
-            2 | 3 => {
-                let input_file: Result<String, io::Error> = fs::read_to_string(&args[1]);
-                if input_file.is_err() {
-                    print_error("Failed to read input file.");
+fn main() {
+    let settings: Settings = get_settings_from_args();
+    let sql_in: String = get_input_sql(&settings);
+    let sql_out: String = get_formatted_sql(sql_in);
+    output_result(&settings, &sql_out);
+}
+
+fn get_settings_from_args() -> Settings {
+    let mut settings: Settings = Settings::new();
+
+    enum ArgType {
+        Input,
+        Output,
+    }
+    let mut arg_type: Option<ArgType> = None;
+
+    for arg in env::args().skip(1) {
+        match arg.as_str() {
+            "-h" | "--help" => {
+                print_help();
+                process::exit(0);
+            }
+            "-u" | "--upper" => {
+                if settings.case.is_some() {
+                    print_error("Case setting already defined.");
+                    print_help();
                     process::exit(1);
                 }
-                sql = Some(input_file.unwrap());
-
-                if args.len() == 3 {
-                    output_path = Some(args[2].clone());
+                settings.case = Some(CaseSetting::Upper);
+            }
+            "-l" | "--lower" => {
+                if settings.case.is_some() {
+                    print_error("Case setting already defined.");
+                    print_help();
+                    process::exit(1);
                 }
+                settings.case = Some(CaseSetting::Lower);
             }
-            _ => {
-                print_error("Invalid arguments provided.");
-                print_help();
-                process::exit(1);
+            "-i" | "--input" => {
+                if arg_type.is_some() {
+                    print_error("Invalid arguments provided.");
+                    print_help();
+                    process::exit(1);
+                }
+                arg_type = Some(ArgType::Input);
             }
+            "-o" | "--output" => {
+                if arg_type.is_some() {
+                    print_error("Invalid arguments provided.");
+                    print_help();
+                    process::exit(1);
+                }
+                arg_type = Some(ArgType::Output);
+            }
+            _ => match arg_type {
+                Some(ArgType::Input) => {
+                    if settings.input.is_some() {
+                        print_error("Input was already defined.");
+                        print_help();
+                        process::exit(1);
+                    }
+                    settings.input = Some(arg);
+                    arg_type = None;
+                }
+                Some(ArgType::Output) => {
+                    if settings.output.is_some() {
+                        print_error("Output was already defined.");
+                        print_help();
+                        process::exit(1);
+                    }
+                    settings.output = Some(arg);
+                    arg_type = None;
+                }
+                None => {
+                    print_error("Invalid arguments provided.");
+                    print_help();
+                    process::exit(1);
+                }
+            },
         }
-    } else {
-        let input_text: Result<String, io::Error> = io::read_to_string(stdin);
-        if input_text.is_err() {
-            print_error("Failed to read input to string.");
-            process::exit(1);
-        }
-        sql = Some(input_text.unwrap());
     }
 
-    if sql.is_none() {
-        print_error("Failed to get SQL to format.");
+    return settings;
+}
+
+fn get_input_sql(settings: &Settings) -> String {
+    let sql_input: Result<String, io::Error>;
+    let stdin: Stdin = io::stdin();
+    if stdin.is_terminal() {
+        if settings.input.is_none() {
+            print_error("Input file not provided.");
+            print_help();
+            process::exit(1);
+        }
+
+        sql_input = fs::read_to_string(settings.input.as_ref().unwrap());
+    } else {
+        sql_input = io::read_to_string(stdin);
+    }
+
+    if sql_input.is_err() {
+        print_error("Failed to read input of SQL.");
         process::exit(1);
     }
 
-    let sql: String = sql.unwrap();
+    return sql_input.unwrap();
+}
 
-    let formatted_sql: String = get_formatted_sql(sql);
-
-    if output_path.is_some() {
-        match fs::write(output_path.unwrap(), formatted_sql) {
+fn output_result(settings: &Settings, sql_out: &String) {
+    if settings.output.is_some() {
+        match fs::write(settings.output.as_ref().unwrap(), sql_out) {
             Ok(_) => (),
             Err(_) => {
                 print_error("Failed to write to output path.");
@@ -73,24 +155,26 @@ fn main() {
             }
         }
     } else {
-        println!("{}", formatted_sql);
+        println!("{}", sql_out);
     }
-
-    process::exit(0);
 }
 
 fn print_help() {
     println!(
         "Format SQL
 
-usage: sqlfmt {}<input file path>{} {}[output file path]{}
+usage: sqlfmt {}-i <input file path>{} {}-o [output file path]{}
        {}<input stream>{} | sqlfmt
 
 notes: {}required{}, {}optional{}
        output is printed to stdout if no output file provided
 
 options:
-    {}-h{}, {}--help{} Show this message",
+    {}
+    {}
+    {}
+    {}
+    {}",
         FONT_CYAN,
         FONT_RESET,
         FONT_MAGENTA,
@@ -101,11 +185,19 @@ options:
         FONT_RESET,
         FONT_MAGENTA,
         FONT_RESET,
-        FONT_YELLOW,
-        FONT_RESET,
-        FONT_YELLOW,
-        FONT_RESET,
+        get_help_parameter('h', "help", "Show this message"),
+        get_help_parameter('i', "input", "Input file path"),
+        get_help_parameter('o', "output", "Output file path"),
+        get_help_parameter('u', "upper", "Uppercase keywords"),
+        get_help_parameter('l', "lower", "Lowercase keywords"),
     );
+}
+
+fn get_help_parameter(letter: char, word: &str, description: &str) -> String {
+    format!(
+        "    {}-{}{}, {}--{}{}\t{}",
+        FONT_YELLOW, letter, FONT_RESET, FONT_YELLOW, word, FONT_RESET, description
+    )
 }
 
 fn print_error(msg: &str) {
