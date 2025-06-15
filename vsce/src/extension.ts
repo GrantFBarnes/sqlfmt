4dc12dcbc406c50c97ae3ce0f465fa14da6d3414
Grant Barnes <gfbarnes78@gmail.com>
2025-06-15 10:51:34 -0500

    reorganize vs code extension

--- vsce/src/extension.ts
+++ vsce/src/extension.ts
@@ -9,88 +9,109 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("sqlfmt.sqlfmt", () => {
+  const sqlfmt = vscode.commands.registerCommand("sqlfmt.sqlfmt", () => {
     const editor = vscode.window.activeTextEditor;
-    const selection = editor?.selection;
-    if (!selection) return;
-
-    let textRange: vscode.Range | null = null;
-    if (selection.isEmpty) {
-      let firstLine = editor.document.lineAt(0);
-      let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-      textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-    } else {
-      textRange = new vscode.Range(
-        selection.start.line,
-        selection.start.character,
-        selection.end.line,
-        selection.end.character
-      );
+    if (editor == null) {
+      vscode.window.showErrorMessage("Could not find active editor.");
+      return;
     }
 
-    const sql_in = editor.document.getText(textRange);
-
-    let args: string[] = [];
-
-    let config = vscode.workspace.getConfiguration();
-
-    if (!config.get("sqlfmt.useConfigFile")) {
-      if (config.get("sqlfmt.replaceNewlines")) {
-        args.push("-n");
-      }
-
-      switch (config.get("sqlfmt.changeKeywordCase")) {
-        case "uppercase":
-          args.push("-u");
-          break;
-
-        case "lowercase":
-          args.push("-l");
-          break;
-
-        default:
-          break;
-      }
-
-      if (config.get("sqlfmt.useTabs")) {
-        args.push("-t");
-      } else {
-        let spaceCount = config.get("sqlfmt.setSpaceCount");
-        if (typeof spaceCount == "number") {
-          args.push("-s");
-          args.push(spaceCount.toString());
-        }
-      }
-
-      let charCount = config.get("sqlfmt.setCharCount");
-      if (typeof charCount == "number") {
-        args.push("-c");
-        args.push(charCount.toString());
-      }
+    const range = getRangeToFormat();
+    if (range == null) {
+      vscode.window.showErrorMessage("Could not gather text to format.");
+      return;
     }
 
-    let p = cp.spawn("sqlfmt", args);
-    p.stdin.write(sql_in);
-    p.stdin.end();
+    const sqlIn = editor.document.getText(range);
+    const args = getSqlFmtArguments();
+    const process = cp.spawn("sqlfmt", args);
+    process.stdin.write(sqlIn);
+    process.stdin.end();
 
-    p.stdout.on("data", (sql_out: any) => {
+    process.stdout.on("data", (sqlOut: any) => {
       editor.edit((editBuilder) => {
-        editBuilder.replace(textRange, sql_out.toString());
+        editBuilder.replace(range, sqlOut.toString());
       });
-      vscode.window.showInformationMessage("Code is formatted");
+      vscode.window.showInformationMessage("SQL is formatted.");
     });
 
-    p.stderr.on("data", (data: any) => {
+    process.stderr.on("data", (data: any) => {
       vscode.window.showErrorMessage(`stderr: ${data}`);
     });
 
-    p.on("close", (code: any) => {
+    process.on("close", (code: any) => {
       console.log(`child process exited with code ${code}`);
     });
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(sqlfmt);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
+
+function getRangeToFormat(): vscode.Range | null {
+  let range: vscode.Range | null = null;
+
+  const editor = vscode.window.activeTextEditor;
+  const selection = editor?.selection;
+  if (!selection) return range;
+
+  if (selection.isEmpty) {
+    const firstLine = editor.document.lineAt(0);
+    const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+    range = new vscode.Range(firstLine.range.start, lastLine.range.end);
+  } else {
+    range = new vscode.Range(
+      selection.start.line,
+      selection.start.character,
+      selection.end.line,
+      selection.end.character
+    );
+  }
+
+  return range;
+}
+
+function getSqlFmtArguments(): string[] {
+  let args: string[] = [];
+
+  const config = vscode.workspace.getConfiguration();
+
+  if (!config.get("sqlfmt.useConfigFile")) {
+    if (config.get("sqlfmt.replaceNewlines")) {
+      args.push("-n");
+    }
+
+    switch (config.get("sqlfmt.changeKeywordCase")) {
+      case "uppercase":
+        args.push("-u");
+        break;
+
+      case "lowercase":
+        args.push("-l");
+        break;
+
+      default:
+        break;
+    }
+
+    if (config.get("sqlfmt.useTabs")) {
+      args.push("-t");
+    } else {
+      let spaceCount = config.get("sqlfmt.setSpaceCount");
+      if (typeof spaceCount == "number") {
+        args.push("-s");
+        args.push(spaceCount.toString());
+      }
+    }
+
+    let charCount = config.get("sqlfmt.setCharCount");
+    if (typeof charCount == "number") {
+      args.push("-c");
+      args.push(charCount.toString());
+    }
+  }
+
+  return args;
+}
